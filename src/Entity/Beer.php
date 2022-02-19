455f23d65cb1532f7db36c104c9b02077744d058
Dannel <danneljt@gmail.com>
2022-02-19 21:26:15 +0100

    create beer entity

---
+++ src/Entity/Beer.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\BeerRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ApiResource(
+ *     collectionOperations={"get"},
+ *     itemOperations={"get"}
+ *     normalizationContext={"groups"={"beer:read"}},
+ * )
+ * @ORM\Entity(repositoryClass=BeerRepository::class)
+ */
+class Beer
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @Groups("beer:read")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups("beer:read")
+     *
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups("beer:read")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=40, nullable=true)
+     * @Groups("beer:read")
+     */
+    private $first_brewed;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups("beer:read")
+     */
+    private $tagline;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Groups("beer:read")
+     */
+    private $image_url;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getFirstBrewed(): ?string
+    {
+        return $this->first_brewed;
+    }
+
+    public function setFirstBrewed(?string $first_brewed): self
+    {
+        $this->first_brewed = $first_brewed;
+
+        return $this;
+    }
+
+    public function getTagline(): ?string
+    {
+        return $this->tagline;
+    }
+
+    public function setTagline(?string $tagline): self
+    {
+        $this->tagline = $tagline;
+
+        return $this;
+    }
+
+    public function getImageUrl(): ?string
+    {
+        return $this->image_url;
+    }
+
+}
